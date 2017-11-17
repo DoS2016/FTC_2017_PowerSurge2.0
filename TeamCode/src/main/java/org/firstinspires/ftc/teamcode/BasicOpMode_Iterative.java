ac6aa0d0cca597af0d05dcc28926da71c2aefb9c
powersurge-luke <33765561+powersurge-luke@users.noreply.github.com>
2017-11-17 17:05:34 -0600

    Touch Sensor for the grabNabbers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
@@ -32,8 +32,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorDigitalTouch;
 
 
 @TeleOp(name = "TeleOp Basic")
@@ -53,7 +56,7 @@ public class BasicOpMode_Iterative extends OpMode
     private Servo rightServo = null;
     private Servo armLeftServo = null;
     private Servo armRightServo = null;
-
+    private DigitalChannel touchSensor = null;
             /*
      * Code to run ONCE when the driver hits INIT
      */
@@ -72,6 +75,7 @@ public class BasicOpMode_Iterative extends OpMode
         rightServo = hardwareMap.get(Servo.class, "right_servo");
         armLeftServo = hardwareMap.get(Servo.class, "arm_servo_blue");
         armRightServo = hardwareMap.get(Servo.class, "arm_servo_red");
+        touchSensor = hardwareMap.get(DigitalChannel.class, "touch_Sensor");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
@@ -107,9 +111,14 @@ public class BasicOpMode_Iterative extends OpMode
         armLeftServo.setPosition(0.8);
         armRightServo.setPosition(0.1);
 
-        if(gamepad2.y) {
-            grabNabberLeft.setPower(100);
+        if(gamepad2.b) {
+            if(touchSensor.getState()){
+                grabNabberLeft.setPower(100);
             grabNabberRight.setPower(-100);
+        }}
+        if(gamepad2.x) {
+            grabNabberLeft.setPower(-100);
+            grabNabberRight.setPower(100);
         }
 
         if (gamepad2.a) {
